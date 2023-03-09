48ef488618d3aa540f2eaaa64e6f7498af8ec4eb
ooion <114280369+ooion@users.noreply.github.com>
2023-03-09 17:29:32 +0800

    Init

---
+++ blogist/a.py
@@ -0,0 +1,29 @@
+import os
+import glob
+# import markdown
+
+if not os.path.exists('public'):
+    os.mkdir('public')
+
+for f in glob.iglob('blog/*.md'):
+    with open(f, 'r') as file:
+        raw = file.read()
+        # html = markdown.markdown(raw)
+
+    file_name = os.path.basename(f)
+    destination = os.path.join("public", os.path.splitext(file_name)[0] + ".html")
+
+    with open(destination, 'w') as file:
+        file.write(r'''<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>My Great Site</title>
+</head>
+<body>
+''')
+        
+        file.write(raw)
+        file.write(r'''
+</body>
+</html>''')
